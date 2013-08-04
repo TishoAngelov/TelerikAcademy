21431ea4d02c318f359293a7128d3b97f5751b45
Tihomir Arahangelov <angelov.tisho@gmail.com>
2013-08-04 20:43:35 +0300

    .init

---
+++ 01.CSharp/C# 1/1. IntroductionToProgramming/09. PrintFirstTenMembOfSequence/PrintFirstTenMembOfSequence.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Write a program that prints the first 10 
+ * members of the sequence: 2, -3, 4, -5, 6, -7, ...
+*/
+using System;
+
+class PrintFirstTenMembOfSequence
+{
+    static void Main()
+    {
+        string sequence = null;
+
+        Console.WriteLine("Print the first ten members of the sequence: 2, -3, 4, -5, 6, -7, ...");
+        for (int i = 2; i < 12; i++)
+        {
+            if (i % 2 == 0)
+            {
+                sequence += i + " ";
+            }
+            else
+            {
+                sequence += -i + " ";
+            }
+        }
+        Console.WriteLine("The full sequence is: " + sequence);
+    }
+}
---
+++ 01.CSharp/C# 1/1. IntroductionToProgramming/12. PrintAgeAfter10Years/PrintAgeAfter10Years.cs
@@ -0,0 +1,20 @@
+﻿/*
+Write a program to read your age from the 
+console and print how old you will be after 10 years.
+*/
+
+using System;
+
+class PrintAgeAfter10Years
+{
+    static void Main()
+    {
+        int yourAge;
+        int ageAfter10Years;
+
+        Console.Write("Please write your age: ");
+        yourAge = int.Parse(Console.ReadLine());
+        ageAfter10Years = yourAge + 10;
+        Console.WriteLine("After 10 years you will be " + ageAfter10Years + " years old!");
+    }
+}
