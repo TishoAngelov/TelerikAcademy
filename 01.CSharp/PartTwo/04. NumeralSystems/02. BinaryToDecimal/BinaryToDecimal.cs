ffd85f27158cc0c846fedfe572ae5d8067b7f8a3
Tihomir Arahangelov <angelov.tisho@gmail.com>
2013-08-05 21:25:25 +0300

    .init

---
+++ 01.CSharp/PartTwo/04. NumeralSystems/02. BinaryToDecimal/BinaryToDecimal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+class BinaryToDecimal
+{
+    // Write a program to convert binary numbers to their decimal representation.
+
+    static void Main()
+    {
+        Console.Write("Enter one binary number (32 digits of 1 or 0): ");
+        string binNum = Console.ReadLine();
+
+        BigInteger decNum = 0;
+
+        for (int i = binNum.Length - 1; i >= 0 ; i--)
+        {
+            int currDigit = Convert.ToInt32(binNum[binNum.Length - i - 1]) - 48;
+
+            if (currDigit != 0 && currDigit != 1)
+            {
+                Console.WriteLine("Your number contains incorrect digits! Only 1 and 0 are allowed!\n");
+
+                return;
+            }
+
+            decNum += currDigit * (BigInteger)Math.Pow(2, i);
+        }
+
+        Console.WriteLine("To decimal: {0}", decNum);
+        Console.WriteLine();
+    }
+}
---
+++ 01.CSharp/PartTwo/04. NumeralSystems/03. DecimalToHexadecimal/DecimalToHexadecimal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class DecimalToHexadecimal
+{
+    // Write a program to convert decimal numbers to their hexadecimal representation.
+
+    static void Main()
+    {
+        Console.Write("Enter one decimal number: ");
+        int decNum = int.Parse(Console.ReadLine());
+
+
+
+        Console.WriteLine();
+    }
+}
