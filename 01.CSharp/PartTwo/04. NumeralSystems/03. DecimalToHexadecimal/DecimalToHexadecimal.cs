0726cd394bf07e3f4ebd58ce1080b18cb415909e
Tihomir Arahangelov <angelov.tisho@gmail.com>
2013-08-06 13:17:33 +0300

    Update

--- 01.CSharp/PartTwo/04. NumeralSystems/03. DecimalToHexadecimal/DecimalToHexadecimal.cs
+++ 01.CSharp/PartTwo/04. NumeralSystems/03. DecimalToHexadecimal/DecimalToHexadecimal.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class DecimalToHexadecimal
 {
@@ -6,11 +7,70 @@ class DecimalToHexadecimal
 
     static void Main()
     {
-        Console.Write("Enter one decimal number: ");
+        Console.Write("Enter one decimal number (positive): ");
         int decNum = int.Parse(Console.ReadLine());
 
+        Console.WriteLine("\nHexadecimal representation");
 
+        if (decNum == 0)
+        {
+            Console.Write(0);
+        }
+        // for positive numbers
+        else if (decNum > 0)
+        {
+            List<string> toHex = new List<string>();
 
+            while (decNum > 0)
+            {
+                switch (decNum % 16)
+                {
+                    case 10:
+                        toHex.Add("A");
+                        break;
+
+                    case 11:
+                        toHex.Add("B");
+                        break;
+
+                    case 12:
+                        toHex.Add("C");
+                        break;
+
+                    case 13:
+                        toHex.Add("D");
+                        break;
+
+                    case 14:
+                        toHex.Add("E");
+                        break;
+
+                    case 15:
+                        toHex.Add("F");
+                        break;
+
+                    default:
+                        toHex.Add((decNum % 16).ToString());
+                        break;
+                }
+                
+                decNum = decNum / 16;
+            }
+
+            toHex.Reverse();
+
+            // print the result
+            for (int i = 0; i < toHex.Count; i++)
+            {
+                Console.Write(toHex[i]);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Incorrect number!");
+        }
+
+        Console.WriteLine();
         Console.WriteLine();
     }
 }
