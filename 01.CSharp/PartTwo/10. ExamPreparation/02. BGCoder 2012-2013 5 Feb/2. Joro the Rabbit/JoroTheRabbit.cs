9fb1e71a217fe5a7a8458310483cf116f001ee32
Tihomir Arahangelov <angelov.tisho@gmail.com>
2013-10-10 21:40:36 +0300

    Update

---
+++ 01.CSharp/PartTwo/10. ExamPreparation/02. BGCoder 2012-2013 5 Feb/2. Joro the Rabbit/JoroTheRabbit.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+class JoroTheRabbit
+{
+    static void Main()
+    {
+        char[] separators = { ',', ' ' };
+        string[] jumpingField = Console.ReadLine().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+        int[] numbers = new int[jumpingField.Length];
+
+        for (int i = 0; i < numbers.Length; i++)
+		{
+            numbers[i] = int.Parse(jumpingField[i]);
+		}
+
+        int maxLength = int.MinValue;
+        int currLength = 0;
+        int currJumpNum = 0;
+        int prevJumpNum = 0;
+        //bool[] visited;
+        int startIndex = 0;
+
+        for (int j = 0; j < jumpingField.Length; j++)
+        {
+            for (int step = 1; step <= jumpingField.Length; step++)
+            {
+                //visited = new bool[jumpingField.Length];
+
+                prevJumpNum = int.MinValue;
+                startIndex = j + step - 1;
+                int i = startIndex;
+
+                if (i >= jumpingField.Length)
+                {
+                    i = i - jumpingField.Length;
+                }
+
+                currLength = 0;
+
+                do
+                {
+                    currJumpNum = numbers[i];
+
+                    if (currJumpNum <= prevJumpNum)
+                    {
+                        break;
+                    }
+
+                    prevJumpNum = currJumpNum;
+                    //visited[i] = true;
+                    
+                    currLength++;
+
+                    i += step;
+
+                    if (i >= jumpingField.Length)
+                    {
+                        i = i - jumpingField.Length;
+                    }                    
+
+                } while (i != startIndex);   // !visited[i]
+
+                maxLength = Math.Max(maxLength, currLength);
+            }
+        }
+
+        Console.WriteLine(maxLength);
+    }
+}
