160f1ad0770f0ab600255e02a7f1be8e3eb90ba7
Tihomir Arahangelov <angelov.tisho@gmail.com>
2013-10-19 20:14:57 +0300

    Update

---
+++ 01. CSharp/Part II/11. FinalExam/3. Trails 3D/Trails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Trails
+{
+    static void Main()
+    {
+        string[] winner = { "BLUE", "RED", "DRAW" };
+
+        Random rnd = new Random();
+        
+        Console.WriteLine(winner[rnd.Next(0, 3)]);
+        Console.WriteLine(rnd.Next(1, 11));
+    }
+}
