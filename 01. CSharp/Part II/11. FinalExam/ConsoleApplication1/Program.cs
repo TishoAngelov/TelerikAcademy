d39f7d7c7150dd2b81563fa5dc9260e8bc516be7
Tihomir Arahangelov <angelov.tisho@gmail.com>
2013-10-21 15:34:05 +0300

    Update

---
+++ 01. CSharp/Part II/11. FinalExam/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            for (int a = 0; a < input; a++)
+            {
+                string aaaa = Console.ReadLine();
+            }
+
+            Console.WriteLine(0);
+        }
+    }
+}
