c51d29c77bdb6ef49fda2bec5f1b62a3cefa847a
Tihomir Arahangelov <angelov.tisho@gmail.com>
2013-11-17 14:30:47 +0200

    Update

---
+++ 02. PHP/Homework-5/books_from_author.php
@@ -0,0 +1,79 @@
+<!--Header-->
+<?php
+$currAuthorName = $_GET['author_name'];
+$pageTitle = 'Книги от '.$currAuthorName;
+include 'header.php';
+?>
+
+<!--Body-->
+<?php
+// Return to page with all books if the author name is empty
+if ($currAuthorName == "" || $currAuthorName == null) {
+    header('Location: index.php');
+    exit;
+}
+
+echo '<h1>Книги от '.$currAuthorName.'</h1>';
+
+// Check if the authors exists
+$query = 'SELECT author_id FROM authors
+          WHERE author_name = "'.$currAuthorName.'"';
+$completedQuery = mysqli_query($connection, $query);
+
+if ($completedQuery->num_rows <= 0) {
+    echo '<h1>Несъществуващ автор!</h1>';
+}
+else {
+    $query = 'SELECT books.book_id, books.book_title, authors.author_name 
+            FROM books INNER JOIN books_authors
+            ON books.book_id = books_authors.book_id
+            INNER JOIN authors
+            ON books_authors.author_id = authors.author_id
+            WHERE books_authors.book_id IN (
+                       SELECT books_authors.book_id
+                       FROM books_authors INNER JOIN authors
+                       ON books_authors.author_id = authors.author_id
+                       WHERE authors.author_name = "' . $currAuthorName . '")';
+    $completedQuery = mysqli_query($connection, $query);
+
+    if (!$completedQuery) {
+        include 'error_in_query.php';
+    }
+    else {
+        if ($completedQuery->num_rows <= 0) {
+            echo '<h1>Този автор все още няма книги!</h1>';
+        }
+        else {       
+            echo '<form method = "GET">
+                    <table border="2">
+                        <tr><td><b>Книги</b></td><td>Автори</td></tr>';
+
+            $result = array();
+
+            while ($row = $completedQuery->fetch_assoc()){
+                $result[$row['book_id']]['book'] = $row['book_title'];
+                $result[$row['book_id']]['authors'][] = $row['author_name'];            
+            }
+            
+            foreach ($result as $key => $currBook) {
+                echo '<tr><td><a href="book.php?book_title='.$currBook['book'].'&book_id='.$key.'">'.$currBook['book'].'</a></td>                    
+                          <td>';
+
+                foreach ($currBook['authors'] as $currAuthor) {
+                    echo '<a href="books_from_author.php?author_name='.$currAuthor.'">'.$currAuthor.'</a> ';
+                }
+
+                echo '</td></tr>';
+            }
+
+            echo '</table>
+                </form>';
+        }    
+    }
+}
+?>
+
+<!--Footer-->
+<?php
+include 'footer.php';
+?>
